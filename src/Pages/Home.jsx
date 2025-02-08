c0def2627615a54167532030825a521203667225
Ashraful Islam <theashrafislam@gmail.com>
2025-02-09 00:24:17 +0600

    admin verify learning done

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,11 +6,18 @@ import toast from 'react-hot-toast';
 
 const Home = () => {
     const [data, setData] = useState([]);
-    const {user, loading} = useContext(AuthContext);
+    const { user, loading } = useContext(AuthContext);
+
+    const [adminData, setAdminData] = useState({});
+    const getAdminData = async () => {
+        const response = await axios.get('http://localhost:5000/admin-api', { withCredentials: true })
+        setAdminData(response?.data?.data)
+        console.log(response);
+    }
 
     // console.log('hello',document.cookie)
     useEffect(() => {
-        if(!user?.email) return 
+        if (!user?.email) return
         axios.get(`http://localhost:5000/secret-data?email=${user?.email}`, { withCredentials: true })
             .then(res => {
                 setData(res?.data?.data)
@@ -19,9 +26,10 @@ const Home = () => {
             .catch(error => {
                 // alert(error?.response?.status)
                 toast.error(error?.response?.status)
-            })
+            });
+        getAdminData()
     }, [user?.email]);
-    if(loading){
+    if (loading) {
         return <p className='text-2xl text-center text-red-500 font-bold mt-16'>Loading...</p>
     }
     return (
@@ -35,6 +43,17 @@ const Home = () => {
                         <p className="text-gray-600">{user.email}</p>
                     </div>
                 ))}
+            </div>
+
+            <div>
+                <h1 className='text-center text-2xl mb-3'>For Admin
+                </h1>
+                <div className='border-2 p-3 bg-gray-400 text-white'>
+                    <h1 className='text-xl text-red-500 font-bold'>Name: {adminData?.name}</h1>
+                    <h1 className='text-xl text-red-500 font-bold'>Email: {adminData?.email}</h1>
+                    <h1 className='text-xl text-red-500 font-bold'>Passwrod: {adminData?.password}</h1>
+                    <h1 className='text-xl text-red-500 font-bold'>Role: {adminData?.role}</h1>
+                </div>
             </div>
         </div>
     );
